59fee0ff47c050c66f4ecb018b37be956ca1528d
josemanuelcordon <josemanuelcordoncastillo01@gmail.com>
2024-02-27 21:03:31 +0100

    Creatednew form

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,15 +1,44 @@
-import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useAuthContext } from "../context/useAuthContext";
+import { useEffect, useState } from "react";
+import { getNews } from "../api/usersApi";
 
 const News = () => {
   const { username } = useAuthContext();
+  const [news, setNews] = useState([]);
 
   useEffect(() => {
-    console.log(username)
-  }, [])
-  
+    const fetchApi = async () => {
+      const response = await getNews();
+      setNews(response);
+    };
+    fetchApi();
+  }, []);
 
-  return <div>{username && <button type="button">Crear Noticia</button>}</div>;
+  return (
+    <main className="min-h-[calc(100vh-160px-112px)] p-10 max-w-7xl mx-auto flex flex-col items-center">
+      {username && (
+        <Link
+          className="py-3 px-8 rounded-xl bg-blue-400 hover:bg-blue-800 text-white"
+          to="/create/new"
+        >
+          Crear Noticia
+        </Link>
+      )}
+      {news && (
+        <section className="w-full rounded-md border-slate-700">
+          {news.map((news) => {
+            return (
+              <div key={news.id} className="w-full p-4">
+                <h2 className="text-xl font-bold">{news.title}</h2>
+                <p className="text-gray-700">{news.description}</p>
+              </div>
+            );
+          })}
+        </section>
+      )}
+    </main>
+  );
 };
 
 export default News;
--- src/pages/router/router.jsx
+++ src/pages/router/router.jsx
@@ -4,6 +4,7 @@ import Home from "../Home";
 import LoginPage from "../LoginPage";
 import Contact from "../Contact";
 import News from "../News";
+import CreateNew from "../CreateNew";
 
 export const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ export const router = createBrowserRouter([
         element: <Contact />,
       },
       { path: "/noticias", element: <News /> },
+      { path: "/create/new", element: <CreateNew /> },
     ],
   },
   {
